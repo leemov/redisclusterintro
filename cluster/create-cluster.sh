d2b381c2795788d8ccb58cb9ddc05fab6f307187
Yustinus Arjuna <yustinus.putra@tokopedia.com>
2021-02-11 09:12:03 +0700

    update apps

--- cluster/create-cluster.sh
+++ cluster/create-cluster.sh
@@ -1,6 +1,6 @@
 #------------ bootstrap the cluster nodes --------------------
 
-start_cmd='redis-server --port 6379 --cluster-enabled yes --cluster-config-file nodes.conf --cluster-node-timeout 5000 --appendonly yes'
+start_cmd='redis-server --port 6379 --cluster-enabled yes --requirepass risktechacademy --cluster-config-file nodes.conf --cluster-node-timeout 5000 --appendonly yes'
 redis_image='redis:5.0-rc'
 network_name='risktechacademy'
 
@@ -10,18 +10,18 @@ echo $network_name " created"
 #---------- create the cluster ------------------------
 
 for port in `seq 6379 6384`; do \
- docker run -d --name "redis-"$port -p $port:6379 --net $network_name $redis_image $start_cmd;
+ docker run -d --rm --name "redis-"$port -p $port:6379 --net $network_name $redis_image $start_cmd;
  echo "created redis cluster node redis-"$port
 done
 
 cluster_hosts=''
 
 for port in `seq 6379 6384`; do \
- hostip=`docker inspect -f '{{(index .NetworkSettings.Networks "redis_cluster_net").IPAddress}}' "redis-"$port`;
+ hostip=`docker inspect -f '{{(index .NetworkSettings.Networks "risktechacademy").IPAddress}}' "redis-"$port`;
  echo "IP for cluster node redis-"$port "is" $hostip
  cluster_hosts="$cluster_hosts$hostip:6379 ";
 done
 
 echo "cluster hosts "$cluster_hosts
 echo "creating cluster...."
-echo 'yes' | docker run -i --rm --net $network_name $redis_image redis-cli --cluster create $cluster_hosts --cluster-replicas 1;
+echo 'yes' | docker run -i --rm --net $network_name $redis_image redis-cli -a risktechacademy --cluster  create $cluster_hosts --cluster-replicas 1;
